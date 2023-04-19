d3a7e30b1301890518f474f7668b62fe780bb17c
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2023-04-19 11:11:16 +0530

    Add files via upload

---
+++ string_palindrom.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class string_palindrom {
+    public static boolean pal(String str) {
+        for (int i=0;i<str.length()/2;i++) {
+            int n = str.length();
+            if (str.charAt(i)!=str.charAt(n-i-1)) {
+                return false;
+            }
+                }
+                return true;
+    }
+    public static void main(String args[]) {
+        String str ="BOf";
+        System.out.println(pal(str));
+    }
+}
